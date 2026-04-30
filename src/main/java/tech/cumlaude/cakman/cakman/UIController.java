7a948b5fe70541196c5269e6be0a9afbde2eb2bc
Azvyl <67502532+Azvyl@users.noreply.github.com>
2026-04-30 07:36:30 +0700

    More button effect
    Menambahkan efek bersinar ke button
    Menambahkan music off (sementara)

--- src/main/java/tech/cumlaude/cakman/cakman/UIController.java
+++ src/main/java/tech/cumlaude/cakman/cakman/UIController.java
@@ -2,36 +2,40 @@ package tech.cumlaude.cakman.cakman;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class UIController {
     @FXML
-    private Button btnScore;
-    @FXML
-    private Button btnPlay;
-    @FXML
-    private Button btnExit;
-    @FXML
-    private Button btnMusic;
+    private Button btnPlay, btnScore, btnExit, btnMusic;
 
     @FXML
     public void initialize() {
-        addHoverEffect(btnScore);
-        addHoverEffect(btnPlay);
-        addHoverEffect(btnExit);
-        addHoverEffect(btnMusic);
+        Button[] buttons = {btnPlay, btnScore, btnExit, btnMusic};
+
+        for (Button b : buttons) {
+            if (b != null) addHoverEffect(b);
+        }
     }
 
     private void addHoverEffect(Button button) {
+        ColorAdjust bright = new ColorAdjust();
+        bright.setBrightness(0.2);
+
         button.setOnMouseEntered(e -> {
             button.setScaleX(1.1);
             button.setScaleY(1.1);
+            button.setEffect(bright);
         });
 
         button.setOnMouseExited(e -> {
             button.setScaleX(1.0);
             button.setScaleY(1.0);
+            button.setEffect(null);
         });
 
+        //TODO: Audio Feedback
         button.setOnMousePressed(e -> {
             button.setScaleX(0.9);
             button.setScaleY(0.9);
@@ -46,5 +50,17 @@ public class UIController {
     @FXML
     private void onExitClick() {
         System.exit(0);
+    }
+
+    //TODO: Music Manager
+    private boolean isMuted = false;
+    private Image imgMusicOn = new Image(getClass().getResourceAsStream("images/music_on.png"));
+    private Image imgMusicOff = new Image(getClass().getResourceAsStream("images/music_off.png"));
+
+    @FXML
+    private void onMusicClick() {
+        isMuted = !isMuted;
+        ImageView view = (ImageView) btnMusic.getGraphic();
+        view.setImage(isMuted ? imgMusicOff : imgMusicOn);
     }
 }
